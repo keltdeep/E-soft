0d256a1d9b01017805ad179ee64e19f59ea6bddd
kelt <keltdeep@gmail.com>
2020-07-12 19:09:05 +0500

    mailgun test

--- config/mail.php
+++ config/mail.php
@@ -33,14 +33,25 @@ return [
     |
     */
 
+//    'mailers' => [
+//        'smtp' => [
+//            'transport' => 'smtp',
+//            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+//            'port' => env('MAIL_PORT', 587),
+//            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+//            'username' => env('MAIL_USERNAME'),
+//            'password' => env('MAIL_PASSWORD'),
+//            'timeout' => null,
+//            'auth_mode' => null,
+//        ],
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
             'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
             'port' => env('MAIL_PORT', 587),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
+            'username' => env('MAILGUN_SMTP_LOGIN'),
+            'password' => env('MAILGUN_SMTP_PASSWORD'),
             'timeout' => null,
             'auth_mode' => null,
         ],
